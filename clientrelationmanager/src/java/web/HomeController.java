dbfaaa6e89d07dd92ca8ace8c73667afecda90c5
Carter Fliss <fcf007@msn.com>
2017-02-07 18:35:08 -0600

    reverted changes due to 404 error

--- clientrelationmanager/src/java/web/HomeController.java
+++ clientrelationmanager/src/java/web/HomeController.java
@@ -19,7 +19,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class HomeController {
-    @RequestMapping("/home")
+    @RequestMapping("/")
     public ModelAndView viewhome(){
         return new ModelAndView("index");
     }
